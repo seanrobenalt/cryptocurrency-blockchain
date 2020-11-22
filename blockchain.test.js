f36ea1d4a44c4821b2936bda3df793620f5395ca
Sean Robenalt <srob0722@gmail.com>
2020-11-21 23:38:35 -0500

    chain validation'

--- blockchain.test.js
+++ blockchain.test.js
@@ -2,7 +2,11 @@ const Blockchain = require('./blockchain');
 const Block = require('./block');
 
 describe('Blockchain', () => {
-  const blockchain = new Blockchain();
+  let blockchain = new Blockchain();
+
+  beforeEach(() => {
+    blockchain = new Blockchain();
+  });
 
   it('contains a `chain` Array instance', () => {
     expect(blockchain.chain).toBeInstanceOf(Array);
@@ -16,5 +20,45 @@ describe('Blockchain', () => {
     const newData = 'foo bar';
     blockchain.addBlock({ data: newData });
     expect(blockchain.chain[blockchain.chain.length-1].data).toEqual(newData);
+  });
+
+  describe('isValidChain()', () => {
+    describe('when the chain does not start with the genesis block', () => {
+      it('returns false', () => {
+        blockchain.chain[0] = { data: 'fake-genesis' };
+
+        expect(Blockchain.isValidChain(blockchain.chain)).toBe(false);
+      });
+    });
+
+    describe('when the chain starts with the genesis block and has multiple blocks', () => {
+      beforeEach(() => {
+        blockchain.addBlock({ data: 'Blueberries' });
+        blockchain.addBlock({ data: 'Strawberries' });
+        blockchain.addBlock({ data: 'Bananas' });
+      });
+      
+      describe('and a lastHash reference has changed', () => {
+        it('returns false', () => {
+          blockchain.chain[2].lastHash = 'broken-lastHash';
+
+          expect(Blockchain.isValidChain(blockchain.chain)).toBe(false);
+        });
+      });
+
+      describe('and the chain contains a block with an invalid field', () => {
+        it('returns false', () => {
+          blockchain.chain[2].data = 'some-hacker-data';
+
+          expect(Blockchain.isValidChain(blockchain.chain)).toBe(false);
+        }); 
+      });
+
+      describe('and the chain does not contain any invalid blocks', () => {
+        it('returns true', () => {
+          expect(Blockchain.isValidChain(blockchain.chain)).toBe(true);
+        });
+      });
+    });
   });
 });

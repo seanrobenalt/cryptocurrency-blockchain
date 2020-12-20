48ad234492391bf1e6b738e441e37f55bc7b339e
Sean Robenalt <srob0722@gmail.com>
2020-12-20 10:19:08 -0500

    get valid transactions

--- wallet/transaction-pool.test.js
+++ wallet/transaction-pool.test.js
@@ -32,4 +32,36 @@ describe('TransactionPool', () => {
       ).toBe(transaction);
     });
   });
+
+  describe('validTransactions()', () => {
+    let validTransactions, errorMock;
+
+    beforeEach(() => {
+      validTransactions = [];
+      errorMock = jest.fn();
+      global.console.error = errorMock;
+
+      for (let i=0; i<0; i++) {
+        transaction = new Transaction({
+          senderWallet,
+          recipient: 'any-recipient',
+          amount: 30
+        });
+
+        if (i%3===0) {
+          transaction.input.amount = 999999;
+        } else if (i%3===1) {
+          transaction.input.signature = new Wallet().sign('foo');
+        } else {
+          validTransactions.push(transaction);
+        }
+
+        transactionPool.setTransaction(transaction);
+      }
+    });
+
+    it('returns valid transactions', () => {
+      expect(transactionPool.validTransactions()).toEqual(validTransactions);
+    });
+  });
 });
